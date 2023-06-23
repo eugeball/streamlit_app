1d192d4e40556e395bbc62796e0651fa31275b27
Eugenia Ball <111295809+eugeball@users.noreply.github.com>
2023-06-23 19:24:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,3 @@ my_fruit_list= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dab
 streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("pick:", list(my_fruit_list.index),['Avocado','Strawberries']
-
-
