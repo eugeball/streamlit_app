1ba00c86fd9cea97d777dead1e5ee8bc2b46cd1d
Eugenia Ball <111295809+eugeball@users.noreply.github.com>
2023-06-23 18:45:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit
 
 streamlit.title('Euge es la mas linda ahr')
 
-🥣 🥗 🐔 🥑🍞
 
-streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.header('🥑🍞Menú de desayuno')
+streamlit.text('🥑🍞Omega 3 y avena con arándanos')
+streamlit.text('🥑🍞Batido de col rizada, espinacas y rúcula')
+streamlit.text('🥑🍞Huevo de gallinas camperas hervidas')
