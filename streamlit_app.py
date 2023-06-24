8f2d34b812c6b73bc91cac8b606251297c488c24
Eugenia Ball <111295809+eugeball@users.noreply.github.com>
2023-06-24 13:58:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,3 @@
-''' import streamlit
-import pandas as pd
-streamlit.title('Euge es la mas linda ahr')
-
-
-streamlit.header('🥑🍞Menú de desayuno')
-streamlit.text('🥑🍞Omega 3 y avena con arándanos')
-streamlit.text('🥑🍞Batido de col rizada, espinacas y rúcula')
-streamlit.text('🥑🍞Huevo de gallinas camperas hervidas')
-
-
-streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
-
-my_fruit_list= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
-
-fruit_selected = streamlit.multiselect("Pick:", list(my_fruit_list.index)
-fruit_to_show = my_fruit_list.loc[fruit_selected]
-streamlit.dataframe(fruits_to_show)
-
-#nueva seccion de display fruityvice
-import requests
-fruityvice_response =requests.get ("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
-'''
-
 import streamlit as st
 import pandas as pd
 import requests
@@ -47,5 +20,8 @@ st.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
 
-
+#take the json version of response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
 
