a59665fc59a938b50d84a929f53a3f1c2078f0df
Eugenia Ball <111295809+eugeball@users.noreply.github.com>
2023-06-24 13:59:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,29 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.title('Euge es la mas linda ahr')
-st.header('🥑🍞Menú de desayuno')
-st.text('🥑🍞Omega 3 y avena con arándanos')
-st.text('🥑🍞Batido de col rizada, espinacas y rúcula')
-st.text('🥑🍞Huevo de gallinas camperas hervidas')
+def main():
+  st.title('Euge es la mas linda ahr')
+  st.header('🥑🍞Menú de desayuno')
+  st.text('🥑🍞Omega 3 y avena con arándanos')
+  st.text('🥑🍞Batido de col rizada, espinacas y rúcula')
+  st.text('🥑🍞Huevo de gallinas camperas hervidas')
 
-st.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+  st.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-st.dataframe(my_fruit_list)
+  my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+  st.dataframe(my_fruit_list)
 
-fruit_selected = st.multiselect("Pick:", list(my_fruit_list.index))
-fruits_to_show = my_fruit_list.loc[fruit_selected]
-st.dataframe(fruits_to_show)
+  fruit_selected = st.multiselect("Pick:", list(my_fruit_list.index))
+  fruits_to_show = my_fruit_list.loc[fruit_selected]
+  st.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  st.text(fruityvice_response.json())
 
-#take the json version of response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+  #take the json version of response and normalize it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  #output it the screen as a table
+  streamlit.dataframe(fruityvice_normalized)
 
+if __name__ == "__main__":
+    main()
